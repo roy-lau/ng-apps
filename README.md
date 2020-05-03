9fcfac248777c24851802b142b01ee4acf9aa661
roylau <roy-lau@foxmail.com>
2020-05-03 19:20:19 +0800

    ng-apps/hero-app: 🚀deploy

--- README.md
+++
@@ -1,8 +0,0 @@
-# angular 应用仓库
-
-## 仿拉钩网
-
-## 英雄项目
-
-[参考](https://angular.cn/tutorial)
-[地址](./hero-app)
--- lagou-ng1/script/controller/favoriteCtr.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('app').controller('favoriteCtr',['$scope', '$state', '$http', function($scope, $state, $http) {
-	$http.get('public/data/myFavorite.json').success(function(res){
-		$scope.list = res;
-	})
-}])
